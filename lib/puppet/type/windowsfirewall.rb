5cec7cddcf13c92d00f9c9de104d8a11e89ff7e9
Gary Larizza <gary@puppetlabs.com>
2017-05-09 12:38:08 -0400

    They are properties not parameters...
    I incorrectly copied/pasted, so now this makes all the properties
    ACTUAL properties

--- lib/puppet/type/windowsfirewall.rb
+++ lib/puppet/type/windowsfirewall.rb
@@ -27,7 +27,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:allow_inbound_rules) do
+  newproperty(:allow_inbound_rules) do
     desc "Allow inbound rules"
     munge do |value|
       value.downcase
@@ -37,7 +37,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:allow_local_firewall_rules) do
+  newproperty(:allow_local_firewall_rules) do
     desc "Allow local firewall rules"
     munge do |value|
       value.downcase
@@ -47,7 +47,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:allow_local_ipsec_rules) do
+  newproperty(:allow_local_ipsec_rules) do
     desc "Allow local IPsec rules"
     munge do |value|
       value.downcase
@@ -57,7 +57,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:allow_user_apps) do
+  newproperty(:allow_user_apps) do
     desc "Allow user apps"
     munge do |value|
       value.downcase
@@ -67,7 +67,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:allow_user_ports) do
+  newproperty(:allow_user_ports) do
     desc "Allow user ports"
     munge do |value|
       value.downcase
@@ -77,7 +77,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:allow_unicast_response_to_multicast) do
+  newproperty(:allow_unicast_response_to_multicast) do
     desc "Allow unicast response to multicast"
     munge do |value|
       value.downcase
@@ -87,7 +87,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:notify_on_listen) do
+  newproperty(:notify_on_listen) do
     desc "Notify on listen"
     munge do |value|
       value.downcase
@@ -97,7 +97,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:enable_stealth_mode_for_ipsec) do
+  newproperty(:enable_stealth_mode_for_ipsec) do
     desc "Enable stealth mode for IPsec"
     munge do |value|
       value.downcase
@@ -107,7 +107,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:log_file_name) do
+  newproperty(:log_file_name) do
     desc "Log file name"
     munge do |value|
       value.downcase
@@ -117,7 +117,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:log_max_size_kilobytes) do
+  newproperty(:log_max_size_kilobytes) do
     desc "Log max size - in kilobytes"
     munge do |value|
       value.downcase
@@ -127,7 +127,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:log_allowed) do
+  newproperty(:log_allowed) do
     desc "Log allowed"
     munge do |value|
       value.downcase
@@ -137,7 +137,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:log_blocked) do
+  newproperty(:log_blocked) do
     desc "Log blocked"
     munge do |value|
       value.downcase
@@ -147,7 +147,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:log_ignored) do
+  newproperty(:log_ignored) do
     desc "Log ignored"
     munge do |value|
       value.downcase
@@ -157,7 +157,7 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
-  newparam(:disabled_interface_aliases) do
+  newproperty(:disabled_interface_aliases) do
     desc "Disabled interface aliases"
     munge do |value|
       value.downcase
