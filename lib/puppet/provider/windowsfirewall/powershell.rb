3ce01c0a1385de36e2997a26527985afb26df6ea
Gary Larizza <gary@puppetlabs.com>
2017-05-09 12:26:50 -0400

    Add all other attributes
    This commit introduces all the attributes that Powershell can configure for
    the windows firewall zones (as of Server 2012).

--- lib/puppet/provider/windowsfirewall/powershell.rb
+++ lib/puppet/provider/windowsfirewall/powershell.rb
@@ -25,9 +25,22 @@ Puppet::Type.type(:windowsfirewall).provide(:powershell) do
   end
 
   @method_map = {
-    'default_inbound_action'  => 'DefaultInboundAction',
-    'default_outbound_action' => 'DefaultOutboundAction',
-    'notify_on_listen'        => 'NotifyOnListen'
+    'default_inbound_action'              => 'DefaultInboundAction',
+    'default_outbound_action'             => 'DefaultOutboundAction',
+    'allow_inbound_rules'                 => 'AllowInboundRules',
+    'allow_local_firewall_rules'          => 'AllowLocalFirewallRules',
+    'allow_local_ipsec_rules'             => 'AllowLocalIPsecRules',
+    'allow_user_apps'                     => 'AllowUserApps',
+    'allow_user_ports'                    => 'AllowUserPorts',
+    'allow_unicast_response_to_multicast' => 'AllowUnicastResponseToMulticast'
+    'notify_on_listen'                    => 'NotifyOnListen',
+    'enable_stealth_mode_for_ipsec'       => 'EnableStealthModeForIPsec',
+    'log_file_name'                       => 'LogFileName',
+    'log_max_size_kilobytes'              => 'LogMaxSizeKilobytes',
+    'log_allowed'                         => 'LogAllowed',
+    'log_blocked'                         => 'LogBlocked',
+    'log_ignored'                         => 'LogIgnored',
+    'disabled_interface_aliases'          => 'DisabledInterfaceAliases'
   }
 
   # Dynamically create methods from the method_map above
--- lib/puppet/type/windowsfirewall.rb
+++ lib/puppet/type/windowsfirewall.rb
@@ -27,8 +27,138 @@ Puppet::Type.newtype(:windowsfirewall) do
     end
   end
 
+  newparam(:allow_inbound_rules) do
+    desc "Allow inbound rules"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:allow_local_firewall_rules) do
+    desc "Allow local firewall rules"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:allow_local_ipsec_rules) do
+    desc "Allow local IPsec rules"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:allow_user_apps) do
+    desc "Allow user apps"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:allow_user_ports) do
+    desc "Allow user ports"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:allow_unicast_response_to_multicast) do
+    desc "Allow unicast response to multicast"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
   newparam(:notify_on_listen) do
     desc "Notify on listen"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:enable_stealth_mode_for_ipsec) do
+    desc "Enable stealth mode for IPsec"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:log_file_name) do
+    desc "Log file name"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:log_max_size_kilobytes) do
+    desc "Log max size - in kilobytes"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:log_allowed) do
+    desc "Log allowed"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:log_blocked) do
+    desc "Log blocked"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:log_ignored) do
+    desc "Log ignored"
+    munge do |value|
+      value.downcase
+    end
+    def insync?(is)
+      is.downcase == should.downcase
+    end
+  end
+
+  newparam(:disabled_interface_aliases) do
+    desc "Disabled interface aliases"
     munge do |value|
       value.downcase
     end
